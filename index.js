f49d4749c23a0b7ea07c69d33347bf2ea8a46b25
somebody <132383273+NaFo44@users.noreply.github.com>
2025-05-02 14:22:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -185,7 +185,6 @@ client.on("messageCreate", async message => {
   }
 
   // "quoi" → "feur"
-  const lastWord = words[words.length - 1].toLowerCase();
   if (["bonne nuit", "Bonne nuit", "bonne nuit !", "Bonne nuit !"].includes(lastWord)) {
     try {
       await message.channel.send("medbed activé !");

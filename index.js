d650770751b0c41a84bada6cddc96c0a12148138
spectra <nayelfourreau@gmail.com>
2025-11-23 23:46:12 +0100

    dumb af

--- index.js
+++ index.js
@@ -39,9 +39,9 @@ const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
-    // GatewayIntentBits.MessageContent,
+    GatewayIntentBits.MessageContent,
     GatewayIntentBits.GuildMessageReactions,
-    // GatewayIntentBits.GuildMembers
+    GatewayIntentBits.GuildMembers
   ],
 });
 

d4272cd85b70e103b7212052c897f30137480d9a
TARDIgradeS <c.9yyw.sbbru7b5yv2lrd09df85mtl4cdy@protonmail.com>
2025-11-24 00:07:04 +0100

    G-Master, montre-moi tes "fs" !

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+import fs from 'fs';
 import { logsDateSeverity } from "./src/utils.js";
 import { handleMessage, handleMessageUpdate, messageReplyHandler } from "./src/messageHandlers.js";
 import { 
